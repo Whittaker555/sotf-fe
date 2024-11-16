90a98f0150b351b4319708faaf150b513d9835df
george.whittaker <george.whittaker@awaze.com>
2024-11-16 21:18:45 +0000

    fixed build

--- infra/main.tf
+++ infra/main.tf
@@ -121,13 +121,20 @@ resource "aws_alb" "application_load_balancer" {
 
 # Create a security group for the load balancer:
 resource "aws_security_group" "load_balancer_security_group" {
+  name = "load-balancer-security-group"
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"] # Allow traffic in from all sources
   }
-
+  
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow traffic in from all sources
+  }
   egress {
     from_port   = 0
     to_port     = 0
@@ -149,9 +156,25 @@ resource "aws_lb_target_group" "target_group" {
 }
 
 resource "aws_lb_listener" "listener" {
-  load_balancer_arn = "${aws_alb.application_load_balancer.arn}" #  load balancer
+  load_balancer_arn = "${aws_alb.application_load_balancer.arn}" # load balancer
   port              = "80"
   protocol          = "HTTP"
+  default_action {
+    type             = "redirect"
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
+resource "aws_lb_listener" "https_listener" {
+  load_balancer_arn = "${aws_alb.application_load_balancer.arn}" # load balancer
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = "arn:aws:acm:eu-west-2:765465445382:certificate/1a60f08a-a713-4b06-9526-4990b7d63e88" # ACM certificate ARN
   default_action {
     type             = "forward"
     target_group_arn = "${aws_lb_target_group.target_group.arn}" # target group
