53640f05691357550ef1246a2951c5082f9147aa
george.whittaker <george.whittaker@awaze.com>
2024-11-21 19:46:30 +0000

    fix lint

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,27 +1,29 @@
-'use client'
+"use client";
 import { useSession } from "next-auth/react";
 import { signIn, signOut } from "next-auth/react";
 import { useState } from "react";
 
 export default function Home() {
-  const { data: session } = useSession(); 
+  const { data: session } = useSession();
   const [playlists, setPlaylists] = useState<string[]>();
 
-  if(session){
-    fetch('https://api.spotify.com/v1/me/playlists', {
-      method: 'GET',
+  if (session) {
+    fetch("https://api.spotify.com/v1/me/playlists", {
+      method: "GET",
       headers: {
-        'Authorization': `Bearer ${session.accessToken}`
+        Authorization: `Bearer ${session.accessToken}`,
+      },
+    }).then((response) => {
+      if (response.status === 401) {
+        signOut();
+      } else if (response.status === 200) {
+        response.json().then((data) => {
+          setPlaylists(
+            data.items.map((playlist: SpotifyItem) => playlist.name)
+          );
+        });
       }
-    }).then(response => {
-      if(response.status === 401){
-        signOut()
-      } else if(response.status === 200){
-        response.json().then(data => {
-          setPlaylists(data.items.map((playlist: any) => playlist.name))
-        })
-      }
-    })
+    });
   }
 
   if (session) {
@@ -31,7 +33,10 @@ export default function Home() {
         <span className="bold-txt">{session?.user?.name}</span>
         <p className="text-white font-normal text-xl mt-5 mb-2">Playlists</p>
         {playlists?.map((playlist) => (
-          <p key={playlist} className="text-white font-normal text-xl mt-5 mb-2">
+          <p
+            key={playlist}
+            className="text-white font-normal text-xl mt-5 mb-2"
+          >
             - {playlist}
           </p>
         ))}
@@ -54,5 +59,52 @@ export default function Home() {
         </button>
       </div>
     );
+  }
+
+  // export interface SpotifyResponse {
+  //   href: string;
+  //   limit: number;
+  //   next: string | null;
+  //   offset: number;
+  //   previous: string | null;
+  //   total: number;
+  //   items: SpotifyItem[];
+  // }
+  interface SpotifyItem {
+    collaborative: boolean;
+    description: string;
+    external_urls: {
+      spotify: string;
+    };
+    href: string;
+    id: string;
+    images: Array<{
+      url: string;
+      height: number | null;
+      width: number | null;
+    }>;
+    name: string;
+    owner: {
+      external_urls: {
+        spotify: string;
+      };
+      followers: {
+        href: string | null;
+        total: number;
+      };
+      href: string;
+      id: string;
+      type: string;
+      uri: string;
+      display_name: string;
+    };
+    public: boolean;
+    snapshot_id: string;
+    tracks: {
+      href: string;
+      total: number;
+    };
+    type: string;
+    uri: string;
   }
 }
